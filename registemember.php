c4d9b897dbd350dbf4caa48fd0c07984af1d62d1
comscisau-Supachat <s5752410006@sau.ac.th>
2017-03-07 15:19:30 +0700

    Update 2

---
+++ registemember.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Register Member</title>
+</head>
+<body>
+<center>
+	<h1>Register Member</h1>
+	<hr>
+	</center>
+</body>
+</html>
